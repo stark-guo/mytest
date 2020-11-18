f2d3138f534affdd8a7a0f5a9d78db0a4a4a426d
admin <admin@qq.com>
2020-11-18 19:46:06 +0800

    idea第一次提交

--- testdemo/src/test/MainTest.java
+++ testdemo/src/test/MainTest.java
@@ -4,4 +4,8 @@ public class MainTest {
 public static void main(String[] args) {
 	System.out.println("abcdefghijklmnopqrstuvwxyz....");
 }
+
+public void sayHi(){
+	System.out.println("Hello world!!!!!!!!!!");
+}
 }
